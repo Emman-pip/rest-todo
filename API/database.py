2f6718ea6dbc484510652a64dbac2fce2d1b0e73
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-21 15:20:50 +0800

    Fix bug when loading data from mongodb.

--- API/database.py
+++ API/database.py
@@ -1,4 +1,5 @@
-from bson import ObjectId
+from bson import ObjectId, json_util
+from flask import json
 from pymongo import MongoClient
 from datetime import datetime
 from dotenv import load_dotenv
@@ -24,7 +25,7 @@ class Database():
         return self.__database
 
     def getAllTasks(self):
-        return self.__database.find({})
+        return json.loads( json_util.dumps(self.__database.find()))
 
     def insertToDb(self, entry: dict):
         self.__database.insert_one(entry)
